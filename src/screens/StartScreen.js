18d3daac8d58d503c163122ac20b4e58b93d834b
anjola85 <anjola85@gmail.com>
2021-10-17 14:15:52 -0400

    first commit

--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -9,10 +9,8 @@ export default function StartScreen({ navigation }) {
   return (
     <Background>
       <Logo />
-      <Header>Login Template</Header>
-      <Paragraph>
-        The easiest way to start with your amazing application.
-      </Paragraph>
+      <Header>This is Haydac</Header>
+      <Paragraph>A click away from Home...</Paragraph>
       <Button
         mode="contained"
         onPress={() => navigation.navigate('LoginScreen')}
