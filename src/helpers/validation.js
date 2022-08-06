7507d69b09f90a898746c49ea2d2c8735f74255c
anjola85 <anjola85@gmail.com>
2022-08-06 19:00:53 -0400

    Added validation for signup inputs

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -7,29 +7,59 @@ import validator from 'validator'
  * and data - as stored user data in DB
  */
 const loginValidator = (formValues) => {
-  if (
-    formValues.email &&
-    validator.isEmail(formValues.email) &&
-    validator.isAlphanumeric(formValues.password)
-  ) {
-    return true
-  }
-  if (
-    formValues.phone &&
-    validator.isMobilePhone(formValues.phone) &&
-    validator.isAlphanumeric(formValues.password)
-  ) {
-    return true
+  if (passwordValidator(formValues.password)) {
+    return emailValidator(formValues.email) || phoneValidator(formValues.phone)
   }
   return false
 }
 
 /**
- *
+ * This function validates form values for signup form.
  * @param {*} formValues - object containing user data
  * @return {message, data} - an object conatining a message as response from the api
  * and data - as stored user data in DB
  */
-const signupValidator = async (formValues) => {}
+const signupValidator = async (formValues) => {
+  const password = formValues.password,
+    confirmPassword = formValues.confirmPassword
+  if (passwordValidator(password) && password == confirmPassword) {
+    return (
+      validator.isAlphanumeric(formValues.name) &&
+      (emailValidator(formValues.email) || phoneValidator(formValues.phone)) &&
+      validator.isAlphanumeric(formValues.address)
+    )
+  }
+}
+
+/**
+ * Helper methods
+ */
+
+/**
+ * Validates email input
+ * @param {*} email
+ * @returns true if email is valid and false otherwise
+ */
+const emailValidator = (email) => {
+  return email && validator.isEmail(email)
+}
+
+/**
+ * Validates phone input
+ * @param {*} phone
+ * @returns true if phone is valid and false otherwise
+ */
+const phoneValidator = (phone) => {
+  return phone && validator.isMobilePhone(phone)
+}
+
+/**
+ * Validates password input
+ * @param {*} password
+ * @returns true if password is valid and false otherwise
+ */
+const passwordValidator = (password) => {
+  return password && validator.isAlphanumeric(password)
+}
 
 export { loginValidator, signupValidator }
