c8bbec8bff3866493301dad4e686eb4a175423de
Amir Mohamad <amohamad@my.yorku.ca>
2022-10-08 20:43:18 +0000

    Start of browse screen

--- src/screens/browse/BrowseScreen.js
+++ src/screens/browse/BrowseScreen.js
@@ -1,18 +1,36 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { StyleSheet } from 'react-native'
+
+import Screen from '../../components/core/Screen'
+import SearchBar from '../../components/SearchBar'
+
+import { FontAwesome } from '@expo/vector-icons'
 
 export default function BrowseScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Browse Screen</Text>
-    </View>
+    <Screen style={styles.container}>
+      <SearchBar
+        width="95%"
+        height={45}
+        placeHolder="Search for local businesses"
+        searchBarOuterStyle={styles.searchBarOuterStyle}
+        searchBarColor="#eee"
+        iconRightColor="#4F4F4F"
+        iconRight={<FontAwesome name="search" size={20} color="#4F4F4F" />}
+        renderIconLeft={false}
+        renderIconRight={true}
+      />
+    </Screen>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  searchBarOuterStyle: {
+    paddingBottom: 10,
+    alignSelf: 'center',
   },
 })
