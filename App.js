680a2414cf0f1aec358792a4436fa42ab225f381
anjola85 <anjola85@gmail.com>
2023-04-24 23:13:49 -0400

    Fetching business data on app load

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import RootNavigator from './src/navigation'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar'
 import { NavigationContainer } from '@react-navigation/native'
 import AuthContextProvider from './src/contexts/AuthContext'
-import RootNavigator from './src/navigation'
 import { Provider } from 'react-redux'
 import store from './src/redux/store'
+import { fetchBusinesses } from './src/redux/business/businessActions'
 // import 'react-native-dotenv/config'
 
 export default function App() {
+  // Dispatch action to fetch all businesses
+  useEffect(async () => {
+    const fetchData = async () => {
+      console.log('testing')
+      store.dispatch(await fetchBusinesses())
+    }
+    fetchData()
+  }, [])
+
+  console.log(store.getState())
+
+  // check if dispatch was successfull
+  if (store.getState().business) {
+    console.log('successfully fetched businesses from api')
+    console.log(store.getState().business.allBusinesses.message)
+  } else {
+    console.log('fail')
+  }
+
   return (
     <Provider store={store}>
       <GestureHandlerRootView style={{ flex: 1 }}>
